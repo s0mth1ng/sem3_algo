ac833dd6d839d3026cb6a23a5ca5854494efb630
Maxim Ivanov <maximsmthivanov@gmail.com>
2020-11-23 17:28:09 +0300

    refactor

--- strings/A.cpp
+++ strings/A.cpp
@@ -2,32 +2,48 @@
 #include <string>
 #include <vector>
 
-std::vector<int> ComputePrefixFunction(const std::string &text) {
+int GetNextPi(const std::string& text,
+              const std::vector<int>& pi,
+              int prev_pi,
+              char cur_symbol) {
+    int cur_pi = prev_pi;
+    while (cur_pi > 0 && cur_symbol != text[cur_pi]) {
+        cur_pi = pi[cur_pi - 1];
+    }
+    if (text[cur_pi] == cur_symbol) {
+        cur_pi++;
+    }
+    return cur_pi;
+}
+
+std::vector<int> ComputePrefixFunction(const std::string& text) {
     std::vector<int> pi(text.size());
     for (size_t i = 1; i < text.size(); ++i) {
-        int j = pi[i - 1];
-        while (j > 0 && text[i] != text[j])
-            j = pi[j - 1];
-        if (text[i] == text[j]) ++j;
-        pi[i] = j;
+        pi[i] = GetNextPi(text, pi, pi[i - 1], text[i]);
     }
     return pi;
 }
 
-void solve(std::istream &in, std::ostream &out) {
+std::vector<int> GetAllOccurrences(const std::string& text,
+                                   const std::string& pattern) {
+    auto pi = ComputePrefixFunction(pattern);
+    std::vector<int> beginIndices;
+    beginIndices.reserve(text.size());
+    int prev_pi = 0;
+    for (size_t i = 0; i < text.size(); ++i) {
+        prev_pi = GetNextPi(pattern, pi, prev_pi, text[i]);
+        if (prev_pi == (int)pattern.size()) {
+            beginIndices.push_back(i + 1 - pattern.size());
+        }
+    }
+    return beginIndices;
+}
+
+void solve(std::istream& in, std::ostream& out) {
     std::string text, pattern;
     in >> pattern >> text;
-    auto pi = ComputePrefixFunction(pattern);
-    int prev = 0;
-    for (size_t i = 0; i < text.size(); ++i) {
-        int j = prev;
-        while (j > 0 && text[i] != pattern[j])
-            j = pi[j - 1];
-        if (text[i] == pattern[j]) ++j;
-        prev = j;
-        if (prev == (int) pattern.size()) {
-            out << i + 1 - pattern.size() << ' ';
-        }
+    for (auto ind : GetAllOccurrences(text, pattern)) {
+        out << ind << ' ';
     }
 }
 
