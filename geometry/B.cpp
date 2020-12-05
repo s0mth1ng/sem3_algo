2fd6f91d99fd70440de68a72aec3705d313c74fc
Maxim Ivanov <maximsmthivanov@gmail.com>
2020-12-05 20:24:21 +0300

    fixed all; now using SortByAngle func

--- geometry/B.cpp
+++ geometry/B.cpp
@@ -1,22 +1,32 @@
+#include <algorithm>
 #include <array>
-#include <iterator>
+#include <cassert>
 #include <cmath>
 #include <fstream>
 #include <iomanip>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <iterator>
+#include <vector>
 
 class Vector {
-private:
-    std::array<double, 3> coords;
+  private:
+    const static size_t _DIMENSION = 3;
+    std::array<double, _DIMENSION> coords;
 
-public:
+  public:
     static Vector Zero;
 
     Vector() : coords({}) {}
 
     Vector(double x, double y, double z) : coords({x, y, z}) {}
+
+    Vector(const std::initializer_list<double> &newCoords) {
+        if (newCoords.size() > _DIMENSION) {
+            throw std::invalid_argument("Dimension is too big.");
+        }
+        coords.fill(0);
+        std::copy(newCoords.begin(), newCoords.end(), coords.begin());
+    }
 
     double length() const {
         double res = 0;
@@ -27,7 +37,7 @@ public:
     }
 
     bool operator==(const Vector &v) const {
-        for (size_t i = 0; i < 3; ++i) {
+        for (size_t i = 0; i < _DIMENSION; ++i) {
             if (coords[i] != v.coords[i]) {
                 return false;
             }
@@ -35,15 +45,13 @@ public:
         return true;
     }
 
-    bool operator<(const Vector &v) const {
-        return coords < v.coords;
-    }
+    bool operator<(const Vector &v) const { return coords < v.coords; }
 
     double operator[](size_t ind) const { return coords[ind]; }
 
     Vector operator+(const Vector &v) const {
         Vector res;
-        for (size_t i = 0; i < 3; ++i) {
+        for (size_t i = 0; i < _DIMENSION; ++i) {
             res.coords[i] = coords[i] + v.coords[i];
         }
         return res;
@@ -51,7 +59,7 @@ public:
 
     Vector operator-(const Vector &v) const {
         Vector res;
-        for (size_t i = 0; i < 3; ++i) {
+        for (size_t i = 0; i < _DIMENSION; ++i) {
             res.coords[i] = coords[i] - v.coords[i];
         }
         return res;
@@ -59,7 +67,7 @@ public:
 
     double operator*(const Vector &v) const {
         double res = 0;
-        for (size_t i = 0; i < 3; ++i) {
+        for (size_t i = 0; i < _DIMENSION; ++i) {
             res += coords[i] * v.coords[i];
         }
         return res;
@@ -67,7 +75,7 @@ public:
 
     Vector operator*(double a) const {
         Vector res;
-        for (size_t i = 0; i < 3; ++i) {
+        for (size_t i = 0; i < _DIMENSION; ++i) {
             res.coords[i] = coords[i] * a;
         }
         return res;
@@ -75,16 +83,17 @@ public:
 
     Vector operator%(const Vector &v) const {
         Vector res;
-        for (size_t i = 0; i < 3; ++i) {
-            res.coords[i] = coords[(i + 1) % 3] * v.coords[(i + 2) % 3] -
-                            coords[(i + 2) % 3] * v.coords[(i + 1) % 3];
+        for (size_t i = 0; i < _DIMENSION; ++i) {
+            res.coords[i] =
+                coords[(i + 1) % _DIMENSION] * v.coords[(i + 2) % _DIMENSION] -
+                coords[(i + 2) % _DIMENSION] * v.coords[(i + 1) % _DIMENSION];
         }
         return res;
     }
 
     friend std::ostream &operator<<(std::ostream &os, const Vector &v) {
         os << '{';
-        for (size_t i = 0; i < 3; ++i) {
+        for (size_t i = 0; i < _DIMENSION; ++i) {
             if (i) {
                 os << ", ";
             }
@@ -95,49 +104,56 @@ public:
     }
 
     friend std::istream &operator>>(std::istream &is, Vector &v) {
-        for (size_t i = 0; i < 2; ++i) {
+        for (size_t i = 0; i < _DIMENSION; ++i) {
             is >> v.coords[i];
         }
         return is;
     }
+
+    void ScanAs2d(std::istream &is) {
+        is >> coords[0] >> coords[1];
+        coords[2] = 0;
+    }
 };
 
-std::vector<Vector> getConvexHull(std::vector<Vector> &pts) {
-    std::sort(pts.begin(), pts.end());
-    pts.erase(std::unique(pts.begin(), pts.end()), pts.end());
-
-    size_t ind = 0;
-    for (size_t i = 1; i < pts.size(); ++i) {
-        if ((pts[ind][1] == pts[i][1] && pts[ind][0] > pts[i][0]) || (pts[ind][1] < pts[i][1])) {
-            ind = i;
-        }
+void SortByAngle(std::vector<Vector> &pts) {
+    if (pts.empty()) {
+        return;
     }
-
-    std::swap(pts[ind], pts.back());
-    std::vector<Vector> hull;
-    hull.push_back(pts.back());
-    pts.pop_back();
-
-    const auto &p0 = hull.back();
-    std::sort(begin(pts), end(pts), [p0](const auto &lhs, const auto &rhs) {
+    auto it = std::min_element(begin(pts), end(pts),
+                               [](const auto &lhs, const auto &rhs) {
+                                   return std::make_pair(lhs[1], -lhs[0]) <
+                                          std::make_pair(rhs[1], -rhs[0]);
+                               });
+    std::iter_swap(pts.begin(), it);
+    const auto &p0 = pts[0];
+    std::sort(begin(pts) + 1, end(pts), [p0](const auto &lhs, const auto &rhs) {
         auto a = lhs - p0;
         auto b = rhs - p0;
         auto c = a % b;
         if (c[2] == 0) {
             return a.length() > b.length();
         }
-        return c[2] < 0;
+        return c[2] > 0;
     });
+}
 
-    hull.push_back(pts[0]);
-    for (size_t i = 1; i < pts.size(); ++i) {
+std::vector<Vector> getConvexHull(std::vector<Vector> &pts) {
+    if (pts.size() < 3) {
+        throw std::invalid_argument("Number of points is less than 3");
+    }
+    std::sort(pts.begin(), pts.end());
+    pts.erase(std::unique(pts.begin(), pts.end()), pts.end());
+    SortByAngle(pts);
+    std::vector<Vector> hull = {pts[0], pts[1]};
+    for (size_t i = 2; i < pts.size(); ++i) {
         const auto &p3 = pts[i];
         while (hull.size() >= 2) {
             const auto &p1 = hull[hull.size() - 2];
             const auto &p2 = hull.back();
             auto F = p2 - p1;
             auto S = p3 - p2;
-            if ((F % S)[2] > 0) {
+            if ((F % S)[2] < 0) {
                 hull.pop_back();
             } else {
                 break;
@@ -145,27 +161,52 @@ std::vector<Vector> getConvexHull(std::vector<Vector> &pts) {
         }
         hull.push_back(p3);
     }
-
     return hull;
+}
+
+double ComputePerimeter(const std::vector<Vector> &polygon) {
+    double ans = 0;
+    for (size_t i = 0; i < polygon.size(); ++i) {
+        size_t ne = (i + 1) % polygon.size();
+        ans += (polygon[ne] - polygon[i]).length();
+    }
+    return ans;
 }
 
 void solve(std::istream &in, std::ostream &out) {
     size_t n_pts;
     in >> n_pts;
-    std::vector<Vector> pts(std::istream_iterator<Vector>(in), {});
-    auto hull = getConvexHull(pts);
-    double ans = 0;
-    for (size_t i = 0; i < hull.size(); ++i) {
-        size_t ne = (i + 1) % hull.size();
-        ans += (hull[ne] - hull[i]).length();
+    std::vector<Vector> pts(n_pts);
+    for (auto &pt : pts) {
+        pt.ScanAs2d(in);
     }
+    auto hull = getConvexHull(pts);
+    out << std::setprecision(10) << std::fixed << ComputePerimeter(hull);
+}
 
-    out << std::setprecision(10) << std::fixed << ans;
+void TestConstructor() {
+    {
+        Vector v({1, 2, 3});
+        assert(v == Vector(1, 2, 3));
+    }
+    {
+        Vector v({1, 2});
+        assert(v == Vector(1, 2, 0));
+    }
+    {
+        Vector v({1});
+        assert(v == Vector(1, 0, 0));
+    }
+    {
+        Vector v({});
+        assert(v == Vector());
+    }
 }
 
 int main() {
 #ifdef LOCAL
-    std::ifstream fin("input.txt");
+    std::ifstream fin("../input.txt");
+    TestConstructor();
     solve(fin, std::cout);
 #else
     solve(std::cin, std::cout);
